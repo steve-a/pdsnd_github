e05dc72b68a8086995f26f561f1f63339ce3e83f
Steve Ascencio <ascenciost@gmail.com>
2020-01-19 22:29:49 -0500

    docs: Add argument info to all docstrings

--- bikeshare.py
+++ bikeshare.py
@@ -88,7 +88,9 @@ def load_data(city, month, day):
 
 
 def time_stats(df):
-    """Displays statistics on the most frequent times of travel."""
+    """Displays statistics on the most frequent times of travel.
+        Args:
+            df - df - Pandas DataFrame containing city data filtered by month and day"""
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
@@ -108,7 +110,9 @@ def time_stats(df):
 
 
 def station_stats(df):
-    """Displays statistics on the most popular stations and trip."""
+    """Displays statistics on the most popular stations and trip.
+        Args:
+            df - df - Pandas DataFrame containing city data filtered by month and day"""
 
     print('\nCalculating The Most Popular Stations and Trip...\n')
     start_time = time.time()
@@ -128,7 +132,9 @@ def station_stats(df):
 
 
 def trip_duration_stats(df):
-    """Displays statistics on the total and average trip duration."""
+    """Displays statistics on the total and average trip duration.
+        Args:
+            df - df - Pandas DataFrame containing city data filtered by month and day"""
 
     print('\nCalculating Trip Duration...\n')
     start_time = time.time()
@@ -144,7 +150,9 @@ def trip_duration_stats(df):
 
 
 def user_stats(df):
-    """Displays statistics on bikeshare users."""
+    """Displays statistics on bikeshare users.
+        Args:
+            df - df - Pandas DataFrame containing city data filtered by month and day"""
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
