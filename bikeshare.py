f5bb0689175f2dc69bf91edc69d37ed1a53eb1d9
Steve Ascencio <ascenciost@gmail.com>
2020-01-19 23:10:36 -0500

    refactor: Change iteration variables to improve readability

--- bikeshare.py
+++ bikeshare.py
@@ -189,21 +189,21 @@ def raw_data(df):
     answer = input('Would you like to see the first five lines of data? ').lower()
     if answer == 'yes':
         #definte iteration variables
-        i = 0
-        j = 4
+        start = 0
+        end = 4
         #prints the first five rows of the dataframe
-        for x in range(i,j):
-            print(df.loc[x])
+        for num in range(start,end):
+            print(df.loc[num])
             print()
         while True:    
             answer = input('Would you like to see the next five lines? ').lower()
             if answer != 'yes':
                 break
             #If they answer yes adjust the variables of iteration and output the next five rows of data
-            i = j + 1
-            j += 5
-            for x in range(i,j):
-                print(df.loc[x])
+            start = end + 1
+            end += 5
+            for num in range(start,end):
+                print(df.loc[num])
                 print()
         
 
